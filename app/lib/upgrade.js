ac2c3cf053dcb9f06d4e8419f4229f6a9a5491db
co2-git <francoisrvespa@gmail.com>
2016-06-23 04:26:11 -0500

    Fix #97 where Implement upgrade

--- app/lib/upgrade.js
+++ app/lib/upgrade.js
@@ -1,4 +1,5 @@
 import 'babel-polyfill';
+import path from 'path';
 import semver from 'semver';
 import read from './read';
 import changeJSON from './changeJSON';
@@ -6,24 +7,38 @@ import getAppFile from './getAppFile';
 import v0_1_4 from '../migrations/v0.1.4';
 import pkg from '../../package.json';
 
-const versions = {
-  '0.1.4': v0_1_4,
-};
+const migrations = [
+  {version: '0.1.4', migrate: v0_1_4},
+];
+
+let base = getAppFile('');
+
+if (/\/node_modules\/reactors$/.test(base)) {
+  base = base.replace(/\/node_modules\/reactors$/, '');
+}
+
+function getFile(file) {
+  return path.join(base, file);
+}
 
 export default () => new Promise(async (resolve, reject) => {
   try {
     let reactors;
     try {
-      reactors = await read(getAppFile('reactors.json'));
+      reactors = await read(getFile('reactors.json'));
+      reactors = JSON.parse(reactors);
     } catch (error) {
-      reactors = {};
+      reactors = {
+        version: pkg.version,
+      };
     }
-    const updatable = versions.filter(version =>
-      semver.gt(version, reactors.version) && semver.lte(version, pkg.version)
+    const versions = migrations.filter(migration =>
+      semver.gt(migration.version, reactors.version) &&
+        semver.lte(migration.version, pkg.version)
     );
-    updatable.forEach(async (fn) => await fn());
+    versions.forEach(async (migration) => await migration.migrate());
     await changeJSON(
-      getAppFile('reactors.json'),
+      getFile('reactors.json'),
       json => {
         json.version = pkg.version;
       },
--- dist/lib/upgrade.js
+++ dist/lib/upgrade.js
@@ -6,6 +6,10 @@ Object.defineProperty(exports, "__esModule", {
 
 require('babel-polyfill');
 
+var _path = require('path');
+
+var _path2 = _interopRequireDefault(_path);
+
 var _semver = require('semver');
 
 var _semver2 = _interopRequireDefault(_semver);
@@ -34,9 +38,17 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { return step("next", value); }, function (err) { return step("throw", err); }); } } return step("next"); }); }; }
 
-var versions = {
-  '0.1.4': _v2.default
-};
+var migrations = [{ version: '0.1.4', migrate: _v2.default }];
+
+var base = (0, _getAppFile2.default)('');
+
+if (/\/node_modules\/reactors$/.test(base)) {
+  base = base.replace(/\/node_modules\/reactors$/, '');
+}
+
+function getFile(file) {
+  return _path2.default.join(base, file);
+}
 
 exports.default = function () {
   return new Promise(function () {
@@ -47,7 +59,7 @@ exports.default = function () {
             case 0:
               _context3.prev = 0;
               return _context3.delegateYield(regeneratorRuntime.mark(function _callee2() {
-                var reactors, updatable;
+                var reactors, versions;
                 return regeneratorRuntime.wrap(function _callee2$(_context2) {
                   while (1) {
                     switch (_context2.prev = _context2.next) {
@@ -55,32 +67,36 @@ exports.default = function () {
                         reactors = void 0;
                         _context2.prev = 1;
                         _context2.next = 4;
-                        return (0, _read2.default)((0, _getAppFile2.default)('reactors.json'));
+                        return (0, _read2.default)(getFile('reactors.json'));
 
                       case 4:
                         reactors = _context2.sent;
-                        _context2.next = 10;
+
+                        reactors = JSON.parse(reactors);
+                        _context2.next = 11;
                         break;
 
-                      case 7:
-                        _context2.prev = 7;
+                      case 8:
+                        _context2.prev = 8;
                         _context2.t0 = _context2['catch'](1);
 
-                        reactors = {};
+                        reactors = {
+                          version: _package2.default.version
+                        };
 
-                      case 10:
-                        updatable = versions.filter(function (version) {
-                          return _semver2.default.gt(version, reactors.version) && _semver2.default.lte(version, _package2.default.version);
+                      case 11:
+                        versions = migrations.filter(function (migration) {
+                          return _semver2.default.gt(migration.version, reactors.version) && _semver2.default.lte(migration.version, _package2.default.version);
                         });
 
-                        updatable.forEach(function () {
-                          var ref = _asyncToGenerator(regeneratorRuntime.mark(function _callee(fn) {
+                        versions.forEach(function () {
+                          var ref = _asyncToGenerator(regeneratorRuntime.mark(function _callee(migration) {
                             return regeneratorRuntime.wrap(function _callee$(_context) {
                               while (1) {
                                 switch (_context.prev = _context.next) {
                                   case 0:
                                     _context.next = 2;
-                                    return fn();
+                                    return migration.migrate();
 
                                   case 2:
                                     return _context.abrupt('return', _context.sent);
@@ -97,20 +113,20 @@ exports.default = function () {
                             return ref.apply(this, arguments);
                           };
                         }());
-                        _context2.next = 14;
-                        return (0, _changeJSON2.default)((0, _getAppFile2.default)('reactors.json'), function (json) {
+                        _context2.next = 15;
+                        return (0, _changeJSON2.default)(getFile('reactors.json'), function (json) {
                           json.version = _package2.default.version;
                         });
 
-                      case 14:
+                      case 15:
                         resolve();
 
-                      case 15:
+                      case 16:
                       case 'end':
                         return _context2.stop();
                     }
                   }
-                }, _callee2, undefined, [[1, 7]]);
+                }, _callee2, undefined, [[1, 8]]);
               })(), 't0', 2);
 
             case 2:
