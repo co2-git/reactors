7b8d9efb6b69ce82a62fce1ebd0b6db7a4d637e5
co2-git <francoisrvespa@gmail.com>
2016-06-28 16:21:46 -0500

    #121 rewrite init to use async/await instead of promise-sequencer

--- app/lib/init.js
+++ app/lib/init.js
@@ -1,4 +1,4 @@
-import sequencer from 'promise-sequencer';
+import 'babel-polyfill';
 import exec from '../lib/exec';
 import transform from '../lib/transform';
 import changeJSON from '../lib/changeJSON';
@@ -7,7 +7,7 @@ import write from '../lib/write';
 import copy from '../lib/copy';
 import logger from '../lib/logger';
 import getLocalFile from '../lib/getLocalFile';
-import {getAppFile as _getAppFile} from '../lib/getAppFile';
+import _getAppFile from '../lib/getAppFile';
 import pkg from '../../package.json';
 
 export default function init(app) {
@@ -19,93 +19,113 @@ export default function init(app) {
     return _getAppFile(file, app);
   }
 
-  return sequencer(
-    () => logger('Installing React Native'),
-    () => exec(`react-native init ${app}`),
-    () => logger.ok('React Native installed'),
-    () => logger('Update mobile indexes'),
-    () => transform(
-      getLocalFile('templates/index.mobile.js'),
-      transformer,
-      getAppFile('index.ios.js'),
-    ),
-    () => transform(
-      getLocalFile('templates/index.mobile.js'),
-      transformer,
-      getAppFile('index.android.js'),
-    ),
-    () => logger('Create html for the web'),
-    () => transform(
-      getLocalFile('templates/index.html'),
-      transformer,
-      getAppFile('index.html'),
-    ),
-    () => logger('Create JS for the web'),
-    () => transform(
-      getLocalFile('templates/index.web.js'),
-      transformer,
-      getAppFile('index.web.js'),
-    ),
-    () => logger('Create app directory'),
-    () => exec(`mkdir ${app}/app/`),
-    () => logger('Create App'),
-    () => transform(
-      getLocalFile('templates/app/App.js'),
-      transformer,
-      getAppFile('app/App.js'),
-    ),
-    () => logger('Create webpack config file for web/desktop'),
-    () => transform(
-      getLocalFile('templates/webpack.config.js'),
-      transformer,
-      getAppFile('webpack.config.js'),
-    ),
-    () => logger('Create desktop directory'),
-    () => exec(`mkdir ${app}/desktop/`),
-    () => logger('Create html for desktop'),
-    () => transform(
-      getLocalFile('templates/index.desktop.html'),
-      transformer,
-      getAppFile('desktop/index.html'),
-    ),
-    () => logger('Create JS for desktop'),
-    () => copy(
-      getLocalFile('templates/index.desktop.js'),
-      getAppFile('index.desktop.js'),
-    ),
-    () => logger('Create DOM html'),
-    () => copy(
-      getLocalFile('templates/index.dom.html'),
-      getAppFile('index.dom.html'),
-    ),
-    () => logger('Create electron index'),
-    () => copy(
-      getLocalFile('templates/index.electron.js'),
-      getAppFile('index.electron.js'),
-    ),
-    () => logger('Installing npm dependencies'),
-    () => npmInstall(app,
-      'reactors',
-      'react-dom',
-      'babel-loader',
-      'webpack',
-      'babel-preset-react',
-      'babel-preset-es2015',
-      'babel-preset-stage-0',
-      'ignore-loader',
-    ),
-    () => logger('Updating package.json'),
-    () => changeJSON(
-      getAppFile('package.json'),
-      (json) => {
-        json.main = 'index.desktop.js';
-      },
-    ),
-    () => logger('create reactors.json'),
-    () => write(
-      getAppFile('reactors.json'),
-      JSON.stringify({version: pkg.version}),
-    ),
-    () => logger.ok(`Reactors app ${app} successfully created`),
-  );
+  return new Promise(async (resolve, reject) => {
+    try {
+      await logger('Installing React Native');
+      await exec(`react-native init ${app}`);
+      await logger.ok('React Native installed');
+
+      await logger('Update mobile indexes');
+      await transform(
+        getLocalFile('templates/index.mobile.js'),
+        transformer,
+        getAppFile('index.ios.js'),
+      );
+      await transform(
+        getLocalFile('templates/index.mobile.js'),
+        transformer,
+        getAppFile('index.android.js'),
+      );
+
+      await logger('Create html for the web');
+      await transform(
+        getLocalFile('templates/index.html'),
+        transformer,
+        getAppFile('index.html'),
+      );
+
+      await logger('Create JS for the web');
+      await transform(
+        getLocalFile('templates/index.web.js'),
+        transformer,
+        getAppFile('index.web.js'),
+      );
+
+      await logger('Create app directory');
+      await exec(`mkdir ${app}/app/`);
+
+      await logger('Create App');
+      await transform(
+        getLocalFile('templates/app/App.js'),
+        transformer,
+        getAppFile('app/App.js'),
+      );
+
+      await logger('Create webpack config file for web/desktop');
+      await transform(
+        getLocalFile('templates/webpack.config.js'),
+        transformer,
+        getAppFile('webpack.config.js'),
+      );
+
+      await logger('Create desktop directory');
+      await exec(`mkdir ${app}/desktop/`);
+
+      await logger('Create html for desktop');
+
+      await transform(
+        getLocalFile('templates/index.desktop.html'),
+        transformer,
+        getAppFile('desktop/index.html'),
+      );
+
+      await logger('Create JS for desktop');
+      await copy(
+        getLocalFile('templates/index.desktop.js'),
+        getAppFile('index.desktop.js'),
+      );
+
+      await logger('Create DOM html');
+      await copy(
+        getLocalFile('templates/index.dom.html'),
+        getAppFile('index.dom.html'),
+      );
+
+      await logger('Create electron index');
+      await copy(
+        getLocalFile('templates/index.electron.js'),
+        getAppFile('index.electron.js'),
+      );
+
+      await logger('Installing npm dependencies');
+      await npmInstall(app,
+        'reactors',
+        'react-dom',
+        'babel-loader',
+        'webpack',
+        'babel-preset-react',
+        'babel-preset-es2015',
+        'babel-preset-stage-0',
+        'ignore-loader',
+      );
+
+      await logger('Updating package.json');
+      await changeJSON(
+        getAppFile('package.json'),
+        (json) => {
+          json.main = 'index.desktop.js';
+        },
+      );
+
+      await logger('create reactors.json');
+      await write(
+        getAppFile('reactors.json'),
+        JSON.stringify({version: pkg.version}),
+      );
+
+      await logger.ok(`Reactors app ${app} successfully created`);
+    } catch (error) {
+      reject(error);
+    }
+  });
 }
--- dist/lib/init.js
+++ dist/lib/init.js
@@ -5,9 +5,7 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.default = init;
 
-var _promiseSequencer = require('promise-sequencer');
-
-var _promiseSequencer2 = _interopRequireDefault(_promiseSequencer);
+require('babel-polyfill');
 
 var _exec = require('../lib/exec');
 
@@ -43,88 +41,187 @@ var _getLocalFile2 = _interopRequireDefault(_getLocalFile);
 
 var _getAppFile2 = require('../lib/getAppFile');
 
+var _getAppFile3 = _interopRequireDefault(_getAppFile2);
+
 var _package = require('../../package.json');
 
 var _package2 = _interopRequireDefault(_package);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { return step("next", value); }, function (err) { return step("throw", err); }); } } return step("next"); }); }; }
+
 function init(app) {
+  var _this = this;
+
   function transformer(source) {
     return source.replace(/\{app\}/g, app);
   }
 
   function getAppFile(file) {
-    return (0, _getAppFile2.getAppFile)(file, app);
+    return (0, _getAppFile3.default)(file, app);
   }
 
-  return (0, _promiseSequencer2.default)(function () {
-    return (0, _logger2.default)('Installing React Native');
-  }, function () {
-    return (0, _exec2.default)('react-native init ' + app);
-  }, function () {
-    return _logger2.default.ok('React Native installed');
-  }, function () {
-    return (0, _logger2.default)('Update mobile indexes');
-  }, function () {
-    return (0, _transform2.default)((0, _getLocalFile2.default)('templates/index.mobile.js'), transformer, getAppFile('index.ios.js'));
-  }, function () {
-    return (0, _transform2.default)((0, _getLocalFile2.default)('templates/index.mobile.js'), transformer, getAppFile('index.android.js'));
-  }, function () {
-    return (0, _logger2.default)('Create html for the web');
-  }, function () {
-    return (0, _transform2.default)((0, _getLocalFile2.default)('templates/index.html'), transformer, getAppFile('index.html'));
-  }, function () {
-    return (0, _logger2.default)('Create JS for the web');
-  }, function () {
-    return (0, _transform2.default)((0, _getLocalFile2.default)('templates/index.web.js'), transformer, getAppFile('index.web.js'));
-  }, function () {
-    return (0, _logger2.default)('Create app directory');
-  }, function () {
-    return (0, _exec2.default)('mkdir ' + app + '/app/');
-  }, function () {
-    return (0, _logger2.default)('Create App');
-  }, function () {
-    return (0, _transform2.default)((0, _getLocalFile2.default)('templates/app/App.js'), transformer, getAppFile('app/App.js'));
-  }, function () {
-    return (0, _logger2.default)('Create webpack config file for web/desktop');
-  }, function () {
-    return (0, _transform2.default)((0, _getLocalFile2.default)('templates/webpack.config.js'), transformer, getAppFile('webpack.config.js'));
-  }, function () {
-    return (0, _logger2.default)('Create desktop directory');
-  }, function () {
-    return (0, _exec2.default)('mkdir ' + app + '/desktop/');
-  }, function () {
-    return (0, _logger2.default)('Create html for desktop');
-  }, function () {
-    return (0, _transform2.default)((0, _getLocalFile2.default)('templates/index.desktop.html'), transformer, getAppFile('desktop/index.html'));
-  }, function () {
-    return (0, _logger2.default)('Create JS for desktop');
-  }, function () {
-    return (0, _copy2.default)((0, _getLocalFile2.default)('templates/index.desktop.js'), getAppFile('index.desktop.js'));
-  }, function () {
-    return (0, _logger2.default)('Create DOM html');
-  }, function () {
-    return (0, _copy2.default)((0, _getLocalFile2.default)('templates/index.dom.html'), getAppFile('index.dom.html'));
-  }, function () {
-    return (0, _logger2.default)('Create electron index');
-  }, function () {
-    return (0, _copy2.default)((0, _getLocalFile2.default)('templates/index.electron.js'), getAppFile('index.electron.js'));
-  }, function () {
-    return (0, _logger2.default)('Installing npm dependencies');
-  }, function () {
-    return (0, _npmInstall2.default)(app, 'reactors', 'react-dom', 'babel-loader', 'webpack', 'babel-preset-react', 'babel-preset-es2015', 'babel-preset-stage-0', 'ignore-loader');
-  }, function () {
-    return (0, _logger2.default)('Updating package.json');
-  }, function () {
-    return (0, _changeJSON2.default)(getAppFile('package.json'), function (json) {
-      json.main = 'index.desktop.js';
-    });
-  }, function () {
-    return (0, _logger2.default)('create reactors.json');
-  }, function () {
-    return (0, _write2.default)(getAppFile('reactors.json'), JSON.stringify({ version: _package2.default.version }));
-  }, function () {
-    return _logger2.default.ok('Reactors app ' + app + ' successfully created');
-  });
+  return new Promise(function () {
+    var ref = _asyncToGenerator(regeneratorRuntime.mark(function _callee(resolve, reject) {
+      return regeneratorRuntime.wrap(function _callee$(_context) {
+        while (1) {
+          switch (_context.prev = _context.next) {
+            case 0:
+              _context.prev = 0;
+              _context.next = 3;
+              return (0, _logger2.default)('Installing React Native');
+
+            case 3:
+              _context.next = 5;
+              return (0, _exec2.default)('react-native init ' + app);
+
+            case 5:
+              _context.next = 7;
+              return _logger2.default.ok('React Native installed');
+
+            case 7:
+              _context.next = 9;
+              return (0, _logger2.default)('Update mobile indexes');
+
+            case 9:
+              _context.next = 11;
+              return (0, _transform2.default)((0, _getLocalFile2.default)('templates/index.mobile.js'), transformer, getAppFile('index.ios.js'));
+
+            case 11:
+              _context.next = 13;
+              return (0, _transform2.default)((0, _getLocalFile2.default)('templates/index.mobile.js'), transformer, getAppFile('index.android.js'));
+
+            case 13:
+              _context.next = 15;
+              return (0, _logger2.default)('Create html for the web');
+
+            case 15:
+              _context.next = 17;
+              return (0, _transform2.default)((0, _getLocalFile2.default)('templates/index.html'), transformer, getAppFile('index.html'));
+
+            case 17:
+              _context.next = 19;
+              return (0, _logger2.default)('Create JS for the web');
+
+            case 19:
+              _context.next = 21;
+              return (0, _transform2.default)((0, _getLocalFile2.default)('templates/index.web.js'), transformer, getAppFile('index.web.js'));
+
+            case 21:
+              _context.next = 23;
+              return (0, _logger2.default)('Create app directory');
+
+            case 23:
+              _context.next = 25;
+              return (0, _exec2.default)('mkdir ' + app + '/app/');
+
+            case 25:
+              _context.next = 27;
+              return (0, _logger2.default)('Create App');
+
+            case 27:
+              _context.next = 29;
+              return (0, _transform2.default)((0, _getLocalFile2.default)('templates/app/App.js'), transformer, getAppFile('app/App.js'));
+
+            case 29:
+              _context.next = 31;
+              return (0, _logger2.default)('Create webpack config file for web/desktop');
+
+            case 31:
+              _context.next = 33;
+              return (0, _transform2.default)((0, _getLocalFile2.default)('templates/webpack.config.js'), transformer, getAppFile('webpack.config.js'));
+
+            case 33:
+              _context.next = 35;
+              return (0, _logger2.default)('Create desktop directory');
+
+            case 35:
+              _context.next = 37;
+              return (0, _exec2.default)('mkdir ' + app + '/desktop/');
+
+            case 37:
+              _context.next = 39;
+              return (0, _logger2.default)('Create html for desktop');
+
+            case 39:
+              _context.next = 41;
+              return (0, _transform2.default)((0, _getLocalFile2.default)('templates/index.desktop.html'), transformer, getAppFile('desktop/index.html'));
+
+            case 41:
+              _context.next = 43;
+              return (0, _logger2.default)('Create JS for desktop');
+
+            case 43:
+              _context.next = 45;
+              return (0, _copy2.default)((0, _getLocalFile2.default)('templates/index.desktop.js'), getAppFile('index.desktop.js'));
+
+            case 45:
+              _context.next = 47;
+              return (0, _logger2.default)('Create DOM html');
+
+            case 47:
+              _context.next = 49;
+              return (0, _copy2.default)((0, _getLocalFile2.default)('templates/index.dom.html'), getAppFile('index.dom.html'));
+
+            case 49:
+              _context.next = 51;
+              return (0, _logger2.default)('Create electron index');
+
+            case 51:
+              _context.next = 53;
+              return (0, _copy2.default)((0, _getLocalFile2.default)('templates/index.electron.js'), getAppFile('index.electron.js'));
+
+            case 53:
+              _context.next = 55;
+              return (0, _logger2.default)('Installing npm dependencies');
+
+            case 55:
+              _context.next = 57;
+              return (0, _npmInstall2.default)(app, 'reactors', 'react-dom', 'babel-loader', 'webpack', 'babel-preset-react', 'babel-preset-es2015', 'babel-preset-stage-0', 'ignore-loader');
+
+            case 57:
+              _context.next = 59;
+              return (0, _logger2.default)('Updating package.json');
+
+            case 59:
+              _context.next = 61;
+              return (0, _changeJSON2.default)(getAppFile('package.json'), function (json) {
+                json.main = 'index.desktop.js';
+              });
+
+            case 61:
+              _context.next = 63;
+              return (0, _logger2.default)('create reactors.json');
+
+            case 63:
+              _context.next = 65;
+              return (0, _write2.default)(getAppFile('reactors.json'), JSON.stringify({ version: _package2.default.version }));
+
+            case 65:
+              _context.next = 67;
+              return _logger2.default.ok('Reactors app ' + app + ' successfully created');
+
+            case 67:
+              _context.next = 72;
+              break;
+
+            case 69:
+              _context.prev = 69;
+              _context.t0 = _context['catch'](0);
+
+              reject(_context.t0);
+
+            case 72:
+            case 'end':
+              return _context.stop();
+          }
+        }
+      }, _callee, _this, [[0, 69]]);
+    }));
+
+    return function (_x, _x2) {
+      return ref.apply(this, arguments);
+    };
+  }());
 }
