db500463b5aa8f714dacac06b3bb4ac3e8197e86
co2-git <francoisrvespa@gmail.com>
2016-07-06 23:45:38 -0500

    Fix #101

--- dist/migrations/v0.1.4/index.desktop.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var _index = require('./index.dom');
-
-var _index2 = _interopRequireDefault(_index);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-(0, _index2.default)('desktop');
