7ace7520f98a4ede48cf9e7ac8e3018a5a38e427
co2-git <francoisrvespa@gmail.com>
2016-12-17 02:43:30 -0600

    Suave

--- app/index.js
+++ app/index.js
@@ -1,12 +1,12 @@
 export {default} from './API/Core';
 
-export {default as View} from './Component/View';
-export {default as Text} from './Component/Text';
-export {default as ScrollView} from './Component/ScrollView';
 export {default as Image} from './Component/Image';
-export {default as ListView} from './Component/ListView';
 export {default as Link} from './Component/Link';
+export {default as ListView} from './Component/ListView';
+export {default as ScrollView} from './Component/ScrollView';
+export {default as Text} from './Component/Text';
+export {default as View} from './Component/View';
 
-export {default as StyleSheet, StyleRule} from './API/StyleSheet';
-export {default as Gesture} from './API/Gesture';
 export {default as Dimensions} from './API/Dimensions';
+export {default as Gesture} from './API/Gesture';
+export {default as StyleSheet, StyleRule} from './API/StyleSheet';
