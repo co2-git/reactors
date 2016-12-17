508709c0c13efa4088b17eec28d630dbeacf6a29
co2-git <francoisrvespa@gmail.com>
2016-12-17 02:42:44 -0600

    Move up dirs

--- app/index.js
+++ app/index.js
@@ -1,12 +1,12 @@
-export {default} from './src/API/Core';
+export {default} from './API/Core';
 
-export {default as View} from './src/Component/View';
-export {default as Text} from './src/Component/Text';
-export {default as ScrollView} from './src/Component/ScrollView';
-export {default as Image} from './src/Component/Image';
-export {default as ListView} from './src/Component/ListView';
-export {default as Link} from './src/Component/Link';
+export {default as View} from './Component/View';
+export {default as Text} from './Component/Text';
+export {default as ScrollView} from './Component/ScrollView';
+export {default as Image} from './Component/Image';
+export {default as ListView} from './Component/ListView';
+export {default as Link} from './Component/Link';
 
-export {default as StyleSheet, StyleRule} from './src/API/StyleSheet';
-export {default as Gesture} from './src/API/Gesture';
-export {default as Dimensions} from './src/API/Dimensions';
+export {default as StyleSheet, StyleRule} from './API/StyleSheet';
+export {default as Gesture} from './API/Gesture';
+export {default as Dimensions} from './API/Dimensions';
